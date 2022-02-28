c9d4e646f5fd3b92daa578e5f05e520e00bcaf0c
fan0223 <louie8653515@yahoo.com.tw>
2022-02-28 15:50:02 +0800

    2022/02/28

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,3 +1,14 @@
-export default () => {
-  return <h1> Landing page </h1>;
+import axios from 'axios'
+
+const LandingPage = ({currentUser}) => {
+  console.log(currentUser)
+  return <h1> Landing page</h1>;
 };
+
+LandingPage.getInitialProps = async() => {
+  const response = await axios.get('http://ticketing.dev/api/users/currentuser')
+
+  return response;
+};
+
+export default LandingPage;
