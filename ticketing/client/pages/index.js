f15c90275bdf970c7a1d0efa88f79e2f1e5c2470
fan0223 <louie8653515@yahoo.com.tw>
2022-03-13 10:55:46 +0800

    2022/03/13

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,14 +1,14 @@
-import axios from 'axios'
+import axios from 'axios';
+import buildClient from '../api/build-client'
 
-const LandingPage = ({currentUser}) => {
-  console.log(currentUser)
-  return <h1> Landing page</h1>;
-};
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? <h1>You are signed</h1> : <h1>You are NOT signed in</h1>
+}
 
-LandingPage.getInitialProps = async() => {
-  const response = await axios.get('http://ticketing.dev/api/users/currentuser')
-
-  return response;
+LandingPage.getInitialProps = async (context) => {
+  const build = buildClient(context)
+  const {data} = await build.get('/api/users/currentuser')
+  return data
 };
 
 export default LandingPage;
